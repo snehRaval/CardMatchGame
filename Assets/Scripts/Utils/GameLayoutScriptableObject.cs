bb9587deefe853dbb9843924ed10cd2c406e46b3
Sneh Raval <snehraval@mplgaming.com>
2025-09-03 12:08:32 +0530

    SO Generation done

---
+++ Assets/Scripts/Utils/GameLayoutScriptableObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameLayoutSize", menuName = "ScriptableObjects/GameLayoutSize", order = 1)]
+public class GameLayoutScriptableObject : ScriptableObject
+{
+    
+    [System.Serializable]
+    public struct GameLayoutSize
+    {
+        public int rows;
+        public int cols;
+
+        public override string ToString()
+        {
+            return $"{rows}x{cols}";
+        }
+    }
+    
+    /// <summary>
+    /// grid levels (rows, cols) must be even count
+    /// </summary>
+    public List<GameLayoutSize> gridLevels;
+}
+
